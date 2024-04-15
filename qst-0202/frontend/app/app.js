9bc63066b6add1bd8c1b27ee862459d6ecbbf983
eyemovic-nishizawa <nishizawa@eyemovic.com>
2024-04-15 20:35:54 +0900

    feat: add `qst-0202`

---
+++ qst-0202/frontend/app/app.js
@@ -0,0 +1,19 @@
+
+const URL_BACKEND = 'http://localhost:4000';
+
+
+/**
+ * 郵便番号で住所を検索する
+ */
+const searchByZipCode = async () => {
+    const inputtedValue = document.getElementById('input-zip').value;
+    // ここに郵便番号検索の処理を書く
+}
+
+/**
+ * 住所で郵便番号を検索する
+ */
+const searchByAddress = async () => {
+    const inputtedValue = document.getElementById('input-address').value;
+    // ここに住所検索の処理を書く
+}
